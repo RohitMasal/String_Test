a3cdbdc52122799c3b99f43413cd08f18937e7d5
RohitMasal <144650536+RohitMasal@users.noreply.github.com>
2023-10-13 12:20:27 +0530

    Add files via upload

---
+++ CharacterShift.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class CharacterShift {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String shiftedString = shiftCharacters(input);
+        System.out.println("Shifted string: " + shiftedString);
+    }
+
+    public static String shiftCharacters(String input) {
+        StringBuilder shiftedString = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                char shiftedChar = (char) ((c - 'A' - 2 + 26) % 26 + 'A');
+                shiftedString.append(shiftedChar);
+            } else if (Character.isLowerCase(c)) {
+                char shiftedChar = (char) ((c - 'a' - 3 + 26) % 26 + 'a');
+                shiftedString.append(shiftedChar);
+            } else {
+                shiftedString.append(c);
+            }
+        }
+
+        return shiftedString.toString();
+    }
+}
